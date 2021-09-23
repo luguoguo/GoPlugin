34fef83de6cf7dcab02f0163a0f68f68f68c4c7d
byhiorg <i@byhi.org>
2021-09-23 14:56:47 +0800

    init

---
+++ AddHeader.go
@@ -0,0 +1,14 @@
+package GoPlugin
+
+import "net/http"
+
+type AddHeader struct {
+}
+
+func New() http.Handler {
+	return &AddHeader{}
+}
+
+func (this *AddHeader) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("name", "plus")
+}
